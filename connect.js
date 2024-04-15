c9bb50a570f61b9c540ac5ded4385e1bf9dd562e
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-04-15 00:07:21 -0400

    removed db name from connection string

--- connect.js
+++ connect.js
@@ -8,7 +8,6 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  database: process.env.DB,
   port: process.env.PORT,
 });
 
