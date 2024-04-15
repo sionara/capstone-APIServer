4afaf4eeb74b68210ab2b8e1b5e51f5f0f4b2dcc
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-04-15 10:10:45 -0400

    updated cors options to accept all origins

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 4000;
-
 const cors = require("cors");
 
-app.use(cors());
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 
 //SET UP FOR EASIER FORM DATA PARSING
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const registerController = require("./register");
-app.post("/register", registerController.register);
+app.post("/register", registerController.register) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+};
 
 const loginController = require("./login");
 app.post("/login", loginController.login);
