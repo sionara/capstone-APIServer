3a292ae06174eea792eb74ff7d5aecd6b983bcf8
Sion Lee <35111239+sionara@users.noreply.github.com>
2026-04-13 00:34:23 -0400

    Update connect.js
    Added SSL and changed port env variable from PORT -> DB_PORT

--- connect.js
+++ connect.js
@@ -8,8 +8,11 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   database: process.env.DB,
+  ssl: {
+    rejectUnauthorized: false  // required for Aiven
+  }
 });
 
 //check db connection
